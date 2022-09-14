2075ce3420bc46409dc0264d91f8ef4a65b23992
Najib Muhammad <najibm38@gmail.com>
2022-09-14 14:45:42 +0000

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int count = strlen(c);
+	int i;
+	for (i = 0; i < count - 1; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
