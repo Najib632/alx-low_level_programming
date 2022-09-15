b1e08299f5f3dbf55e6ff99f17fe98f8d8d89823
Najib Muhammad <najibm38@gmail.com>
2022-09-15 00:20:33 +0000

    Fix betty coding and documenting style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,14 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: parameter to be checked
+ *
+ * Return: if n is negative return n * -1
+ * otherwise return n
  */
 int _abs(int n)
 {
 	if (n < 0)
 		return (n * -1);
 	else
-		return n;
+		return (n);
 }
