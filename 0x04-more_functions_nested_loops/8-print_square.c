5fdd044c8c6680f30255be9091b62483b78dc869
Najib Muhammad <najibm38@gmail.com>
2022-10-03 22:47:57 +0000

    Fix betty-style error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,17 +7,17 @@
 
 void print_square(int size)
 {
-        int i = 0, j;
+	int i = 0, j;
 
-        if (size > 0)
-        {
-                for (; i < size; i++)
-                {
-                        for (j = 0; j < size; j++)
-                                _putchar(35);
-                        _putchar('\n');
-                }
-        }
-        else
-                _putchar('\n');
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
