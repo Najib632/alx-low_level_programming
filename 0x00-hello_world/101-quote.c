8ce577ce546622d5b3bb24918c19ac8ca2a3df20
Najib Muhammad <najibm38@gmail.com>
2023-08-05 21:00:38 -0700

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	ssize_t message_length = strlen(message);
+	write(2, message, messsage_length);
 	return (1);
 }
