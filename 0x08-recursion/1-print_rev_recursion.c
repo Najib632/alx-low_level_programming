0fa26897009a57d5b8d70246bfa32f930a2614ae
Najib Muhammad <najibm38@gmail.com>
2023-08-10 17:21:08 +0000

    Change conditional statement

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s);
-		_putchar(*s);
+		return;
 	}
 	else
 	{
-		return;
+		_print_rev_recursion(s);
+		_putchar(*s);
 	}
 }
