19ae42797fba58c9df901d3823b65f1e04f6a561
Najib Muhammad <najibm38@gmail.com>
2023-08-13 20:30:02 +0000

    Remove function prototype its file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _radicand - returns the square root of a radical
+ * @n: Radicand
+ * @i: index
+ *
+ * Return: square root of a number
+ */
+int _radicand(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	else if (i * i < n)
+		return (_radicand(n, i + 1));
+	else
+		return (i);
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: radical
  *
