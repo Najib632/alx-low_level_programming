e2a1361ef43d4ac68c92fa02560da9b6eb3ac153
Najib Muhammad <najibm38@gmail.com>
2023-08-15 01:53:42 +0000

    Use stdlib nil instead of NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("Name: %s\n", "\0");
+		printf("Name: %s\n", "(nil)");
 	else
 		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: %s\n", "\0");
+		printf("Owner: %s\n", "(nil)");
 	else
 		printf("Owner: %s\n", d->owner);
 }
