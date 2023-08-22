6f22e1de7f793e98a76e0e1270aac20a9715a099
Najib Muhmmad <najibm38@gmail.com>
2023-08-22 13:46:23 +0100

    Reduce number of tabs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	const char *message =
-						"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t message_length = strlen(message);
 
 	write(2, message, message_length);
