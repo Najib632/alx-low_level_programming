74cf3a01146e6c52f9520e183e93612c064db874
Najib Muhmmad <najibm38@gmail.com>
2023-08-23 14:44:00 +0100

    Fix return statement

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,5 +14,5 @@ int is_prime_number(int n)
 	else if (n == 2 || n - 1 == 2)
 		return (1);
 	else
-		return ((n % 2 != 0) ? n * (n / 2) : is_prime_number(n - 1));
+		return ((n % 2 != 0) ? 1 : is_prime_number(n - 1));
 }
