0b561e98303abe7edd0a804ea1d61bceabfd83d4
Najib Muhammad <najibm38@mail.com>
2023-09-29 23:49:35 +0100

    Puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,20 @@
 #include "main.h"
+
 /**
- * -puts_recursion - */
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: str to be printed
+ *
+ * Return: Recursion
+ **/
 void _puts_recursion(char *s)
 {
-	
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		return;
+	}
 }
