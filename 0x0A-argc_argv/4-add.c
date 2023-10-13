0c686c922e62ed49a83bc9e2fdc565e78d9ada83
Najib Muhammad <najibm38@mail.com>
2023-10-13 02:11:26 +0100

    Use stdlib to solve issues

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * aNum - verify if string is a number
+ * @str: str to be checked
+ *
+ * Return: 0 (SUCCESS), 1 (FAILURE)
+ **/
+int aNum(char *str)
+{
+	int i;
+	int _strlen;
+
+	_strlen = strlen(str);
+	for (i = 0; i < _strlen; i++)
+	{
+		if (isdigit(str[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * main - adds positive numbers
  * @argc: argument count
  * @argv: argument variable
@@ -21,9 +41,9 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		j = atoi(argv[i]);
-		if (j != 0)
+		if (aNum(argv[i]) == 0)
 		{
+			j = atoi(argv[i]);
 			sum += j;
 		}
 		else
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,8 @@
 #define _MAIN_H_
 
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <stdio.h>
 int _putchar(char s);
 
