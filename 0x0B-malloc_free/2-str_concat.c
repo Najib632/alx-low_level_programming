edbb897448bf49535330194fec2edbe8275033a7
Najib Muhammad <najibm38@mail.com>
2023-10-13 11:19:13 +0100

    Refactor code written at night

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,27 +35,20 @@ char *str_concat(char *str1, char *str2)
 	char *ptr_c;
 
 	size_1 = 0, size_2 = 0;
-	if (str1 != NULL)
-		str1 = "";
-	if (str2 != NULL)
-		str2 = "";
 
 	size_str = (size_1 + size_2) - 1;
 	ptr_c = malloc(sizeof(char) * size_str);
 	if (ptr_c != NULL)
 	{
-		i = 0;
-		j = 0;
-
-		for (i = size_1, j = 0; i > j; j++)
+		for (j = 0; j < size_1; j++)
 			if (str1[j] != '\0')
 				ptr_c[j] = str1[j];
 			else
 				ptr_c[j] = (str1[j] == '\0') ? '\0' : str1[j];
 
-		for (i = size_1 - 1, j = 0; i <= size_str; i++, j++)
+		for (i = 0; i <= size_2; i++, j++)
 			if (*str1 != '\0')
-				ptr_c[i] = str2[j];
+				ptr_c[j] = str2[i];
 			else
 				ptr_c[i + 1] = str2[j];
 	}
