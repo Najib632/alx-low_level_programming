6e9257b6e3192c523088547e4d6873890ce93b78
Najib Muhammad <najibm38@mail.com>
2023-10-15 12:38:40 +0100

    Remove clever code syntax

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,8 +30,12 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			*str++ = av[i][j];
-		*str++ = '\n';
+		{
+			*str = av[i][j];
+			str++;
+		}
+		*str = '\n';
+		str++;
 	}
 	return (str);
 }
