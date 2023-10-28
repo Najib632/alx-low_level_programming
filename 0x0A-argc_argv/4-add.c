87d40b609307c59827650e65a7b17e48922860ae
Najib Muhammad <najibm38@mail.com>
2023-10-28 21:33:23 +0100

    Fix logical error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@ int is_num_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (isdigit(str[i]))
+		if (!(str[i] >= '0' || str[i] <= '9'))
 			return (1);
 	}
 	return (0);
@@ -29,27 +29,25 @@ int main(int argc, char *argv[])
 {
 	int sum;
 
-	sum = 0;
-	if (argc == 1)
-	{
-		printf("%d\n", sum);
-	}
-	else
+	if (argc > 1)
 	{
 		int i;
 
+		sum = 0;
 		for (i = 0; i < argc; i++)
 		{
-			if (is_num_string(argv[i]))
+			if (!is_num_string(argv[i]))
+				sum += atoi(argv[i]);
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
 		}
+	}
+	else
+	{
+		sum = 0;
 	}
 	printf("%d\n", sum);
 	return (0);
