5a9c30a8b786bc6859251109474a559b17fe9833
Najib Muhammad <najibm38@gmail.com>
2023-11-03 10:35:15 +0100

    Bug fix: Use memset to intialize it to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _memset -   fills memory with a constant byte
+ * @s: pointer block of memory to fill
+ * @b: value to set
+ * @n: bytes of the memory
+ *
+ * Return: dest
+ **/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
  * _calloc - allocates memory for an array of nmemb elements of size bytes each
  *           and returns a pointer to the allocated memory.
  * @nmemb: number of memory bytes
@@ -19,5 +38,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		if (alloc == NULL)
 			return (NULL);
 
+		_memset(alloc, 0, nmemb * size);
 		return (alloc);
 }
