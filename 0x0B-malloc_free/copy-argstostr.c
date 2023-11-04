6fbdca158f9924c7877785cc1a1bc60dad6a6e1d
Najib Muhammad <najibm38@mail.com>
2023-11-05 00:16:57 +0100

    Bug fix: right memory allocated

--- 0x0B-malloc_free/copy-argstostr.c
+++ 0x0B-malloc_free/copy-argstostr.c
@@ -1,4 +1,52 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter
+ * @str: string to be duplicated
+ *
+ * Return: pointer (SUCCESS), NULL (FAILURE)
+ **/
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+	int j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+	copy = malloc((i + 1) * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
+
+	i++;
+	for (j = 0; j <= i; j++)
+		copy[j] = str[j];
+	return (copy);
+}
+
+/**
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
 
 /**
  * argstostr - concatenates all the arguments
@@ -10,6 +58,7 @@
 char *argstostr(int ac, char **av)
 {
 	char *s;
+	char *c;
 	int i, j, k, strl;
 
 	if (ac < 0 || av == NULL)
@@ -28,15 +77,19 @@ char *argstostr(int ac, char **av)
 	s = malloc(strl * sizeof(char));
 	if (s == NULL)
 		return (NULL);
+
+	_memset(s, '\0', strl * sizeof(char));
 	for (i = 0, k = 0; i < ac; i++)
 	{
-		for (j = 0; k <= strl; j++, k++)
-		{
-			s[k] = av[i][j];
-			if (!s[k])
+		c = _strdup(av[i]);
+		for (strl = 0; c[strl]; strl++)
+			;
+		for (j = 0; j <= strl; j++, k++)
+			if (!c[j])
 				s[k] = '\n';
-		}
+			else
+				s[k] = c[j];
+		free(c);
 	}
-	s[k++] = '\0';
 	return (s);
 }
