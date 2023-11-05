dffff95b815576eb317ba71263251f0b503fb393
Najib Muhammad <najibm38@mail.com>
2023-11-05 02:56:40 +0100

    Bug fix: accessing memory directly leaves no memory leaks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
- *           which contains a copy of the string given as a parameter
- * @str: string to be duplicated
- *
- * Return: pointer (SUCCESS), NULL (FAILURE)
- **/
-char *_strdup(char *str)
-{
-	char *copy;
-	int i;
-	int j;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i]; i++)
-		;
-	copy = malloc(i * sizeof(char));
-	if (copy == NULL)
-		return (NULL);
-
-	for (j = 0; j < i; j++)
-		copy[j] = str[j];
-	return (copy);
-}
-
-/**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
- */
-char *_memset(char *s, char b, unsigned int n)
-
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
 
 /**
  * argstostr - concatenates all the arguments
@@ -66,28 +18,32 @@ char *argstostr(int ac, char **av)
 	strl = 0;
 	for (i = 0; i < ac; i++)
 	{
-		char *c = _strdup(av[i]);
-
-		strl += strlen(c);
-		free(c);
+		for (j = 0; av[i][j]; j++)
+		{
+			strl++;
+		}
 	}
 
-	s = malloc(strl * sizeof(char));
+	strl += ac;
+	s = malloc((strl + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-
-	_memset(s, '\0', strl * sizeof(char));
 	for (i = 0, k = 0; i < ac; i++)
 	{
-		char *c = _strdup(av[i]);
-
-		strl = strlen(c);
-		for (j = 0; j < strl; j++, k++)
+		for (j = 0; k <= strl; j++, k++)
 		{
-			s[k] = c[j];
+			if (k != strl)
+			{
+				if (av[i][j] == '\0')
+					s[k] = '\n';
+				else
+					s[k] = av[i][j];
+			}
+			else
+			{
+				s[k] = '\0';
+			}
 		}
-		s[k++] = '\n';
-		free(c);
 	}
 	return (s);
 }
