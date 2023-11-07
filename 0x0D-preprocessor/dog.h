c86cb9b12cbe9340944cbbad5444fa55a5f574c3
Najib Muhammad <najibm38@mail.com>
2023-11-07 04:46:31 +0100

    Define a new type struct dog

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,17 @@
+#ifndef _STRUCTURES_TYPEDEF_
+#define _STRUCTURES_TYPEDEF_
+
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* _STRUCTURES_TYPEDEF_ */
