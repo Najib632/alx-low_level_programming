ec6ed12712ad661437b7e87401a2760995f55173
Najib Muhammad <najibm38@mail.com>
2023-11-08 06:29:59 +0100

    Write a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: poiter to a dog_t type
+ *
+ * Return: Nothing
+ **/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d);
+}
