82478d7a8e4fff7ee374ebed08e4f7b7a7861d1a
Najib Muhammad <najibm38@mail.com>
2023-11-15 21:41:58 +0100

    Bug fix: did not derefernce pointer comparision

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,14 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (i <= 5)
 	{
-		if (s == ops[i].op)
+		if (*s == *ops[i].op)
 		{
-			return (ops[i].f);
+			if (s != NULL)
+				return (ops[i].f);
+			else
+				return (NULL);
 		}
 		i++;
 	}
