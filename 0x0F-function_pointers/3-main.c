381ad6daad3d61864236b9adf4548b23763c8acc
Najib Muhammad <najibm38@mail.com>
2023-11-15 21:37:00 +0100

    Error: operator not working

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
  * main - performs simple operations
@@ -9,5 +10,24 @@
  **/
 int main(int argc, char *argv[])
 {
+	if (!(argc > 3))
+	{
+		printf("Error\n");
+		exit(98);
+		return (1);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+		return (1);
+	}
+	if ((atoi(argv[3]) == 0) && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+		return (1);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
