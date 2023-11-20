73321363deb906cc7365b3b2e872d3bbfa13cb3a
Najib Muhammad <najibm38@mail.com>
2023-11-21 00:00:06 +0100

    Bug fix: do not print separator at the end of the last number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	for (i = n; i > 0; --i)
+	for (i = 0; i < n; ++i)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	putchar('\n');
