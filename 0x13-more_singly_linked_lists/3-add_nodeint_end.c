a344eb85f66d53c05068e6816db61a1ed87cd83c
Najib Muhammad <najibm38@mail.com>
2023-11-30 19:08:07 +0100

    Write a function that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,26 +9,26 @@
  **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *node;
-	listint_t *current;
+	listint_t *node; /* current node to be alloced*/
+	listint_t *prev_node; /* previous node parser*/
 
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
+	node = malloc(sizeof(listint_t)); /* alloc mem exactly how listint_t is*/
+	if (node == NULL) /* check if mem is not alloced*/
+		return (NULL); /* return is mem is not alloced*/
 
-	node->n = n;
-	node->next = NULL;
+	node->n = n; /* assign n to be stored in the node created*/
+	node->next = NULL; /* indicate that the end of the linked list*/
 
-	if (*head == NULL)
+	if (*head == NULL) /* check if the previous pionter is not empty*/
 	{
-		*head = node;
-		return (node);
+		*head = node; /* point to the current node if empty*/
+		return (node); /* return the current node*/
 	}
 
-	current = *head;
-	while (current != NULL)
-		current = current->next;
+	prev_node = *head; /* point to the previous node*/
+	while (prev_node->next != NULL)/*check next link is not the end of list*/
+		prev_node = prev_node->next; /* point to the next node */
 
-	current->next = node;
+	prev_node->next = node; /*make the previous node point to the */
 	return (node);
 }
