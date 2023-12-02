f79892b94c8055cb65b8c7b6d5d09ca554361221
Najib Muhammad <najibm38@mail.com>
2023-12-02 15:35:13 +0100

    Error: segmentation fault

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes node at index of a listint_t linked list
+ * @head: head node of a listint_t linked list
+ * @index: position of a value in a linked list
+ *
+ * Return: Always 1 (SUCCESS), -1 (FAILURE)
+ **/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *delete_node;
+	listint_t *prev_node;
+	listint_t *next_node;
+
+	next_node = *head;
+	if (next_node && head)
+	{
+		for (i = index; (int)i >= 0; --i)
+		{
+			if (i < index && !next_node->next)
+				return (-1);
+			/*if ((int)i == ((int)index - 1))*/
+			prev_node = delete_node;
+			delete_node = next_node;
+			next_node = next_node->next;
+		}
+		prev_node->next = next_node;
+		free(delete_node);
+		return (1);
+	}
+	return (-1);
+}
