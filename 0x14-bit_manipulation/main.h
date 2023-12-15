33ee84a1bdc074a955e8c0ef3b3d819a9d26742b
Najib Muhammad <najibm38@mail.com>
2023-12-15 22:41:30 +0100

    Write a function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,6 +10,6 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *, unsigned int index);
-
+int clear_bit(unsigned long int *n, unsigned int index);
+int flip_bits(unsigned long int n, unsigned int m);
 #endif /* _MAIN_H_ */
