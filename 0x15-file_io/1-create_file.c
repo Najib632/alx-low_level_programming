d6692b363a36e8f2e6e2507b97a39b699085f665
Najib Muhammad <najibm38@mail.com>
2023-12-21 20:09:42 +0100

    Bug fix: change return value for second check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (!text_content)
 	{
-		return (-1);
+		return (1);
 	}
 	for (text_size = 0; text_content[text_size]; ++text_size)
 		;
