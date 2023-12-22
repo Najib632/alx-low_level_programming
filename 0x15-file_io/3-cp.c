90c50cbd124df6ab6815801d3cf1de65d24d837e
Najib Muhammad <najibm38@mail.com>
2023-12-22 12:45:07 +0100

    Wait till tomorrow

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,8 +60,8 @@ int main(int ac, char *av[])
 		stat = write(outputFd, buff, numRead);
 		if (stat != numRead)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
 		}
 	}
 	check_close(inputFd, outputFd);
