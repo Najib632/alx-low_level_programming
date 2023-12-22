7ed717444c8c259cfa4faab18127732598732236
Najib Muhammad <najibm38@mail.com>
2023-12-22 01:17:48 +0100

    Bug fix: Add checker for output file descriptor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,27 @@
 #define BUF_SIZE 1024
 
 /**
+ * check_close - Checks if closing files was successful.
+ * @inputFd: input file descriptor
+ * @outputFd: output file descriptor
+ *
+ * Return: Nothing
+ **/
+void check_close(int inputFd, int outputFd)
+{
+	if (close(inputFd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close inputFd fd %d\n", inputFd);
+		exit(100);
+	}
+	if (close(outputFd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close outputFd fd %d\n", outputFd);
+		exit(100);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @ac: arg count
  * @av: arg variable
@@ -18,7 +39,10 @@ int main(int ac, char *av[])
 	char buff[BUF_SIZE];
 
 	if (ac != 3)
-		errx(97, "Usage: cp file_from file_to");
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	inputFd = open(av[1], O_RDONLY);
 	if (inputFd == -1)
 	{
@@ -27,6 +51,11 @@ int main(int ac, char *av[])
 	}
 	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
 	outputFd = creat(av[2], filePerms);
+	if (outputFd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	while ((numRead = read(inputFd, buff, BUF_SIZE)) > 0)
 	{
 		stat = write(outputFd, buff, numRead);
@@ -35,16 +64,6 @@ int main(int ac, char *av[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	}
-	if (close(inputFd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close inputFd fd %d\n", inputFd);
-		exit(100);
-	}
-	if (close(outputFd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close outputFd fd %d\n", outputFd);
-		exit(100);
 	}
 	return (0);
 }
