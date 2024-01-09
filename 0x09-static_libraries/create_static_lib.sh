980a7957b5a597c4f1aa1e21b24b94ccd2efd7a5
Najib Muhammad <najibm38@mail.com>
2024-01-09 23:39:28 +0100

    Starting again

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -Werror -Wall -Wextra -pedantic -std=gnu89 -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
--- 0x0A-argc_argv/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-int _putchar(char s);
-
-#endif /* _MAIN_H_ */
