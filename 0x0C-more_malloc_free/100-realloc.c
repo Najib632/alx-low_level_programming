4dd9fa4e53108dfac7d5d3ccc0a721bb52cf863d
Najib Muhammad <najibm38@mail.com>
2024-01-19 10:23:59 +0100

    Bug: fix complete rewrite

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * _realloc - Reallocates a memory block using malloc and free
@@ -10,31 +11,32 @@
  **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_mem = NULL;
-	unsigned int i;
+	void *newptr = malloc(new_size);
+	unsigned int copy_size = old_size < new_size ? old_size : new_size;
 
-	new_mem = malloc(new_size);
-	if (new_mem == NULL)
-		return (NULL);
-	if (ptr == NULL)
-		return (new_mem);
-	if (new_size < old_size)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	for (i = 0; i < old_size; ++i)
-	{
-		((char *)new_mem)[i] = ((char *)ptr)[i];
-	}
-	free(ptr);
-	return (new_mem);
+	if (newptr == NULL)
+		return (NULL);
+
+
+	if (copy_size)
+		memcpy(newptr, ptr, copy_size);
+
+	if (old_size > new_size)
+		free(ptr);
+	else
+		free(ptr);
+
+	return (newptr);
 }
