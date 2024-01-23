b2ed0435ec2b573f5029d661a581d6f2277e9aec
Najib Muhammad <najibm38@mail.com>
2024-01-23 13:09:40 +0100

    Works well for odd numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -22,6 +22,33 @@ int is_num_string(char *str)
 }
 
 /**
+ * pop - removes a value at an index
+ * @ptr: pointer to array
+ * @size: size of array
+ * @index: index to remove
+ *
+ * Return: new array
+ **/
+void *pop(void *ptr, unsigned int size, unsigned int index)
+{
+	char *popped, *p_ptr;
+	int i, j;
+
+	if (ptr == NULL)
+		return (NULL);
+	p_ptr = ptr;
+	popped = malloc(size * sizeof(char));
+	if (popped == NULL)
+		return (NULL);
+	for (i = 0, j = 0; i < size; i++)
+	{
+		if (i != index)
+			popped[j++] = p_ptr[i]
+	}
+	return (popped);
+}
+
+/**
  * multiply - multiplies two numbers
  * @str1: multiplicand: left operand
  * @str2: multiplier: right operand
@@ -30,34 +57,48 @@ int is_num_string(char *str)
  *
  * Return: Product
  **/
-void multiply(char *str1, char *str2, size_t size1, size_t size2)
+void *multiply(char *str1, char *str2, size_t size1, size_t size2)
 {
 	int prod, carry, left, right, result;
 	unsigned int write;
-	int i, j, m, size_p = size1 + size2 + 1;
+	int i, j, m, size_p = size1 + size2 + 1, fsize;
 	char *answer = malloc(size_p * sizeof(char));
 
-	carry = 0;
-	prod = 0;
+	carry = prod = 0;
+	fsize = size_p;
 	memset(answer, 0, size_p);
 	for (i = size2 - 1; i >= 0; --i)
 	{
 		right = str2[i] - '0';
-		for (j = size1 - 1; j >= 0; --j)
+		if (i == 0 && right == 0)
+		{
+			printf("0\n");
+			return;
+		}
+		for (j = size1 - 1, m = size_p - 2; j >= 0; --j)
 		{
 			left = str1[j] - '0';
 			prod = (left * right) + carry;
 			result = prod % 10;
-			m = i + j;
 			write = answer[m] == 0 ? result : (answer[m] - '0') + result;
 			answer[m] = (write % 10) + '0';
 			if (write > 9)
-				answer[m - 1] = (write / 10) + '0';
+			{
+				carry = write / 10;
+				write = (answer[m - 1] - '0') + carry;
+				answer[m - 1] = (write % 10) + '0';
+				if (write > 9)
+					answer[m - 2] = (write / 10) + '0';
+			}
 			carry = prod / 10;
 		}
+		size_p--;
 	}
+	if (carry > 0)
+		answer[0] = carry + '0';
+	else
+		answer = pop(answer, fsize - 1, 0);
 	printf("%s", answer);
-	putchar('\n');
 	free(answer);
 }
 
