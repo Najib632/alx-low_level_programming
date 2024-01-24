689ed860bbf26893c9ef40420ee4b409fb33db40
Najib Muhammad <najibm38@mail.com>
2024-01-24 21:55:14 +0100

    Write a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: argument variables
+ *
+ * Return: Always 0 (SUCCESS)
+ **/
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+		return (1);
+	}
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+		return (1);
+	}
+	if ((atoi(argv[3]) == 0) && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+		return (1);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
