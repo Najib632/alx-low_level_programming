93ed45b7f37f31bbcbe03ac997a7e680fd72fdfd
Najib Muhammad <najibm38@mail.com>
2024-02-06 15:15:21 +0100

    Write a function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - This function can free lists with a loop.
+ * @head: linked list to be freed.
+ *
+ * Return: the size of the list that was free'd
+ **/
