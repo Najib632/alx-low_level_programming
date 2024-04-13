638666ca9084c0ec62c28b88acf21afab633db5a
Najib Muhammad <najibm38@mail.com>
2024-04-13 22:03:44 +0100

    Write a function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - will create a hash table of a certain size
+ * @size: size of the array of linked lists
+ *
+ * Return: hash_table (SUCCESS), NULL (FAIL)
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Cannot create hash table");
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = malloc(ht->size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		fprintf(stderr, "Cannot create array");
+		return (NULL);
+	}
+
+	return (ht);
+}
