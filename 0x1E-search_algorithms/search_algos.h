78f7b7da6f08fd7d85fcfe3035cac770d7ab8cd3
Najib Muhammad <najibm38@mail.com>
2024-07-21 05:55:45 +0100

    Linear search algorith

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGO__H_
+#define _SEARCH_ALGO__H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H_ */
