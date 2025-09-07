a0540ba2bbde8a2a6bc1ccda0824add00754b4f4
Najib Muhammad <najibm38@gmail.com>
2025-09-08 00:47:41 +0100

    Update readme

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 
 /**
- * main - Write a program that prints the first 50 Fibonacci numbers,
+ * main - Write a program that prints the first 98 Fibonacci numbers,
  *        starting with `1` and `2`, followed by a new line.
  * Return: Always 0 (Success)
  */
