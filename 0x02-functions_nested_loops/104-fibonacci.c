77c8c105fcfe60ab3960dd708b897d392274c5c2
Najib Muhammad <najibm38@gmail.com>
2025-09-07 23:45:24 +0100

    Original solution to 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,39 +8,38 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int fib, next, m, j;
+	int i, m, j;
+	unsigned long int fib, next;
 
 	next = 1;
 	fib = 0;
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 91; i++)
 	{
-		if (i <= 90)
-		{
-			fib = fib + next;
-			printf("%ld", fib);
-			next = fib > 2 ? fib - next : next;
-			if ((next + fib) > ULONG_MAX)
-			{
-				m = fib % 1000;
-				j = next % 1000;
-				fib = (fib - m) / 1000;
-				next = (next - j) / 1000;
-			}
-		}
-		else
-		{
-			next = (fib + next) + ((m + j) / 1000);
-			printf("%lu%lu", next, (m + j) > 1000 ? (m + j) - 1000 : m + j);
-			fib = next - fib;
-			j = j + m;
-			m = j - m;
-		}
+		fib = fib + next;
+		printf("%ld", fib);
+		next = fib > 2 ? fib - next : next;
 		if ((i + 1) < 98)
 		{
 			printf(", ");
 		}
 	}
+	m = fib % 1000;
+	j = next % 1000;
+	fib = (fib - m) / 1000;
+	next = (next - j) / 1000;
+	do {
+		m = m + j;
+		j = m - j;
+		fib = (fib + next) + (m / 1000);
+		printf("%lu%03d", fib,  (m % 1000));
+		next = (fib - next) - (m / 1000);
+		m = m % 1000;
+		i++;
+		if ((i + 1) < 99)
+		{
+			printf(", ");
+		}
+	} while (i < 98);
 	putchar('\n');
 	return (0);
 }
