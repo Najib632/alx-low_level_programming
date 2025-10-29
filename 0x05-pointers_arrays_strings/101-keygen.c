3f03c810519054085febf48f3eb460e53d97a272
Najib Muhammad <najibm38@gmail.com>
2025-10-29 12:11:30 +0100

    original attempt for keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - generates keygen.
+ *
  * Return: 0 Always.
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	int rchr;
+	int remainder;
+	int subtotal;
+	int count;
+	int T = 0xAD4;
+	int i;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
+	srand((unsigned int) time(NULL));
+	rchr = ((rand() % (126 - 32 + 1)) + 32);
+	count = T / rchr;
+	subtotal = count * rchr;
+	remainder = T - subtotal;
+
+	for (i = 0; i < count; i++)
+		printf("%c", rchr);
+	printf("%c\n", remainder);
 	return (0);
 }
+
