a611d654858111e1318fb664ac121f3ad4f84991
Najib Muhammad <najibm38@gmail.com>
2026-05-20 14:14:25 +0100

    Original attempt to set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -6,15 +6,10 @@
  * set_string - A function that sets the value of a pointer to a char.
  * @s: string buffer
  * @to: set string to
+ *
  * Return: Nothing
  */
 void set_string(char **s, char *to)
 {
-	if (!s)
-	{
-		*s = malloc(strlen(to) + 1);
-		if (!(*s))
-		return;
-	}
 	*s = to;
 }
