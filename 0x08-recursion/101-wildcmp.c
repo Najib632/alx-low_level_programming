8c850d9ef310cc9ea8ede232c0dca2fb23e7af93
Najib Muhammad <najibm38@gmail.com>
2026-07-07 21:41:26 +0100

    Collaborated solution

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,32 +2,24 @@
 
 /**
  * wildcmp - a function that compares two strings
- * @str1: doesn't contain wildcard
- * @str2: contains wildcard
+ * @s1: doesn't contain wildcard
+ * @s2: contains wildcard
  *
  * Return: On Success 1, On Failure 0
  **/
-int wildcmp(char *str1, char *str2)
+int wildcmp(char *s1, char *s2)
 {
-	if (*str1 != *str2)
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == '*')
 	{
-		if (*str2 == '*')
-		{
-			if (*(str1 + 1) != *(str2 + 1))
-			{
-				if (*(str2 + 1) == '*' || *(str1 + 1) == '\0' ||
-					(*(str2 + 2) == '*' && *(str2 + 1) == *str1))
-					return (wildcmp(str1, ++str2));
-				else
-					return (wildcmp(++str1, str2));
-			}
-			else
-				return (wildcmp(++str1, ++str2));
-		}
-		if (*str2 != '*')
-			return (0);
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		return (0);
 	}
-	if (*str2 == '\0' && *str1 == '\0')
-		return (1);
-	return (wildcmp(++str1, ++str2));
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
 }
